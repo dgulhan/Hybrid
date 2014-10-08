f846130a55aa3110a83c5422758a6ba499775edd
Doga <dgulhan@cern.ch>
2014-10-08 11:09:37 +0200

    alpha values changed

--- run_hybrid.sh
+++ run_hybrid.sh
@@ -1,65 +1,63 @@
-# [method][Tc][RAAfit]
-
- g++ hybrid.cc
- 
+ # g++ hybrid.cc
+ g++ `root-config --cflags` hybrid.cc -o hybrid `root-config --libs`
  for Tc in 180 200
   do
-  for quench_method in 1 4 3 
+  for quench_method in 0 1 2  
    do
    for iRAAfit in 0 1 
     do
     if [ "$iRAAfit" = "0" ]; then
      if [ "$Tc" = "200" ]; then 
-      if [ "$quench_method" = "1" ]; then 
+      if [ "$quench_method" = "0" ]; then 
        alpha=3.5927
       fi 
-      if [ "$quench_method" = "4" ]; then 
-       alpha=2.629
+      if [ "$quench_method" = "1" ]; then 
+       alpha=2.7
       fi
-      if [ "$quench_method" = "3" ]; then
-       alpha=0.4759
+      if [ "$quench_method" = "2" ]; then
+       alpha=0.493
       fi
      fi
      if [ "$Tc" = "180" ]; then 
-      if [ "$quench_method" = "1" ]; then 
-       alpha=5.3746
+      if [ "$quench_method" = "0" ]; then 
+       alpha=2.8225
       fi
-      if [ "$quench_method" = "4" ]; then
-       alpha=4.3232
+      if [ "$quench_method" = "1" ]; then
+       alpha=1.9
       fi
-      if [ "$quench_method" = "3" ]; then
-       alpha=4.1035
+      if [ "$quench_method" = "2" ]; then
+       alpha=0.42
       fi
      fi
     fi
-    if [ "$iRAAfit" = "0" ]; then
+    if [ "$iRAAfit" = "1" ]; then
      if [ "$Tc" = "200" ]; then
-      if [ "$quench_method" = "1" ]; then
+      if [ "$quench_method" = "0" ]; then
        alpha=5.3746
       fi 
-      if [ "$quench_method" = "4" ]; then
-       alpha=4.1035
+      if [ "$quench_method" = "1" ]; then
+       alpha=4.35
       fi
-      if [ "$quench_method" = "3" ]; then
-       alpha=0.5709
+      if [ "$quench_method" = "2" ]; then
+       alpha=0.6
       fi
      fi
      if [ "$Tc" = "180" ]; then
-      if [ "$quench_method" = "1" ]; then
+      if [ "$quench_method" = "0" ]; then
        alpha=4.3232
       fi
-      if [ "$quench_method" = "4" ]; then
-       alpha=2.8127
+      if [ "$quench_method" = "1" ]; then
+       alpha=3.1
       fi
-      if [ "$quench_method" = "3" ]; then
-       alpha=0.4714
+      if [ "$quench_method" = "2" ]; then
+       alpha=0.51
       fi
      fi
     fi
     # for iCentrality in {0..8}
     for iCentrality in 0
      do
-     ./a.out $quench_method $iCentrality $alpha $Tc
+     nohup ./hybrid $quench_method $iCentrality $alpha $Tc 0 > run_{$quench_method}_{$iCentrality}_{$alpha}_{$Tc}_0.out & 
     done
    done
   done
