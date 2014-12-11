03a44e45ebc2fbb573fcb52529b3c5726f211847
Doga <dgulhan@cern.ch>
2014-12-11 16:46:14 +0100

    possible fix for mem leak

--- hybrid_photon.cc
+++ hybrid_photon.cc
@@ -41,18 +41,18 @@ int main(int argc, char* argv[]) {
  for(int ifile=0; ifile<1;ifile++){ 
   std::stringstream fpythia;
   // fpythia<<"/afs/cern.ch/work/d/dgulhan/dataQG/maindata_p"<<ifile<<"_1000evts.txt";
-  fpythia<<"photon/PHOT40.txt";
+  // fpythia<<"photon/PHOT40.txt";
   // fpythia<<"/data/dgulhan/Adsdata_pthat50/TREES/full"; 
   // fpythia<<"/data/dgulhan/Adsdata_Photon60/TREES/phot60"<<file_number;
-  // fpythia<<"/data/dgulhan/Adsdata_Photon60/TREES/full";
+  fpythia<<"/data/dgulhan/Adsdata_Photon60/TREES/full";
   // fpythia<<"oneevent_photon.dat";
   DataFile_Parser *file = new DataFile_Parser(fpythia.str().c_str());//pythia file
   cout <<fpythia.str().c_str()<<endl;
   vector <Event> event_vector = file->get_event_vector(); 
 
   std::stringstream fout;
-  // fout << "Outfile_m"<<quench_method<<"_photon60/"<< "datafile_jetfile_" << file_number <<"_method_"<<quench_method<< "_centrality_" << min_centrality_bins[centrality_bin] << "_"<< max_centrality_bins[centrality_bin] << "_factor" << (factor) << "_Tc"<<Tc<<".txt";   
-  fout << "test.txt";   
+  fout << "Outfile_m"<<quench_method<<"_photon60/"<< "datafile_jetfile_" << file_number <<"_method_"<<quench_method<< "_centrality_" << min_centrality_bins[centrality_bin] << "_"<< max_centrality_bins[centrality_bin] << "_factor" << (factor) << "_Tc"<<Tc<<".txt";   
+  // fout << "test.txt";   
   ofstream myfile;
   myfile.open(fout.str().c_str());
 
@@ -78,7 +78,10 @@ int main(int argc, char* argv[]) {
    cout<<"event="<<event<<endl;
    for(int i=0;i<nfrags;i++){
     Fragment * elem=it->get_ieth_fragment(i);
-    if(elem->get_taus()<50) continue;
+    if(elem->get_taus()<50){
+     delete elem;
+     continue;
+    }
     double qE = elem->get_quenched_E();
     double p_quench_factor = elem->get_quenched_E()/elem->get_E();
     double qpx = p_quench_factor*elem->get_px();
@@ -93,6 +96,6 @@ int main(int argc, char* argv[]) {
    myfile<<"END\n";
   }
   myfile.close();
+  delete file;
  }
- fpythia.close();
 }
